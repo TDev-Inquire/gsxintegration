c01b02cdd39650ebf66c873a4eda54d3824def64
TDev-Inquire <64050282+TDev-Inquire@users.noreply.github.com>
2021-10-17 09:13:55 -0400

    Version 0.2 upgrade
    Finished .textContent update
    created the correct array for customer information

--- content2.js
+++ content2.js
@@ -1,8 +1,11 @@
-console.log("target content2 ran");
 
-var storedLegal = chrome.storage.local.get('newStorage', function (items) {
-    console.log(items); 
 
-    document.querySelector("#home-search__textbox").value = items.newStorage.cxname;
-    ///document.getElementById("lastName").value = items.newStorage.src_lname; 
-  });
+var storedLegal = chrome.storage.local.get('cxStorage', function (items) {
+  console.log(items); 
+
+  document.querySelector("#home-search__textbox").value = items.cxStorage.cx_number;
+  ///document.getElementById("lastName").value = items.newStorage.src_lname; 
+
+console.log("target 2nd piece ran");
+
+});
--- content_nomsg.js
+++ content_nomsg.js
@@ -1,16 +1,23 @@
-var cx_name = document.getElementById("custh1").value;
-
-
+var cx_name = document.querySelector("#custh1 > span").textContent;
+var cx_number = document.querySelector("#contact-table > tbody > tr:nth-child(3) > td:nth-child(2) > span").textContent;
+var cx_email = document.querySelector("#contact-table > tbody > tr:nth-child(5) > td:nth-child(2) > a > span").textContent;
+var cx_reference = 
+ =
+var cx_note =
 console.log("source page ran");
 
-var storArray = {
+var cxInfo = {
     cx_name: cx_name,
-   
-    ///cx_problem: cxprblm,
-    ///integ_id: intid
+    cx_number: cx_number,
+    cx_email: cx_email,
+    cx_address: cx_address,
+    cx_note: cx_note,
+    cx_addln: cx_addln,
+    cx_city: cx_city,
+    cx_zip: cx_zip,
+    cx_pchorder: cx_pchorder,
+    cx_reference: cx_reference,
     };
-
-
 chrome.storage.local.set({
-        'newStorage': storArray
+        'cxStorage': cxInfo
         });
