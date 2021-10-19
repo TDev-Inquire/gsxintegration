ef5b9f9c89f6ac0b6b45cfd5d0ca83e7fa80feb9
TDev-Inquire <64050282+TDev-Inquire@users.noreply.github.com>
2021-10-19 09:29:49 -0400

    Changed variable back to normal

--- content2.js
+++ content2.js
@@ -1,17 +1,17 @@
 
 
-var storedLegal = chrome.storage.local.get('cxInfo', function (items) {
+var storedLegal = chrome.storage.local.get('cxStorage', function (items) {
   console.log(items); 
 
-  document.querySelector("#rep_cust__fn").value = items.cxInfo.cx_number;
-  document.querySelector("#rep_cust__fn").value = items.cxInfo.cx_fname;
-  document.querySelector("#rep_cust__ln").value = items.cxInfo.cx_lname;
-  document.querySelector("#rep_cust__em").value = items.cxInfo.cx_email;
-  document.querySelector("#home-search__textbox").value = items.cxInfo.cx_address;
-  document.querySelector("#rep_cust__notes").value = items.cxInfo.cx_note;
-  document.querySelector("#rep_cust__po").value = items.cxInfo.cx_pchorder;
-  document.querySelector("#rep_cust__ref").value = items.cxInfo.cx_reference;
-  document.querySelector("#home-search__textbox").value = items.cxInfo.cx_serial;
+  document.querySelector("#rep_cust__fn").value = items.cxStorage.cx_number;
+  document.querySelector("#rep_cust__fn").value = items.cxStorage.cx_fname;
+  document.querySelector("#rep_cust__ln").value = items.cxStorage.cx_lname;
+  document.querySelector("#rep_cust__em").value = items.cxStorage.cx_email;
+  document.querySelector("#home-search__textbox").value = items.cxStorage.cx_address;
+  document.querySelector("#rep_cust__notes").value = items.cxStorage.cx_note;
+  document.querySelector("#rep_cust__po").value = items.cxStorage.cx_pchorder;
+  document.querySelector("#rep_cust__ref").value = items.cxStorage.cx_reference;
+  document.querySelector("#home-search__textbox").value = items.cxStorage.cx_serial;
   document.querySelector("#rep_cust__class").value = "SINGLE";
   document.querySelector("#rep_cust__st").value = "NC";
 
