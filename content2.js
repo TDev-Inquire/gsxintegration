c6862c9af1f1cd6660d891b2c263f33aa6c036e4
TDev-Inquire <64050282+TDev-Inquire@users.noreply.github.com>
2021-10-26 07:36:01 -0400

    bug update

--- content2.js
+++ content2.js
@@ -10,15 +10,11 @@ var storedLegal = chrome.storage.local.get('cxStorage', function (items) {
   document.querySelector("#rep_cust__notes").value = items.cxStorage.cx_note;
   document.querySelector("#rep_cust__po").value = items.cxStorage.cx_pchorder;
   document.querySelector("#rep_cust__ref").value = items.cxStorage.cx_reference;
-  document.querySelector("#rep_cust__fn").value = items.cxStorage.cx_number;
-  document.querySelector("#rep_cust__fn").value = items.cxStorage.cx_fname;
-  document.querySelector("#rep_cust__ln").value = items.cxStorage.cx_lname;
-  document.querySelector("#rep_cust__em").value = items.cxStorage.cx_email;
-  document.querySelector("#rep_cust__notes").value = items.cxStorage.cx_note;
-  document.querySelector("#rep_cust__po").value = items.cxStorage.cx_pchorder;
-  document.querySelector("#rep_cust__ref").value = items.cxStorage.cx_reference;
   document.querySelector("#rep_cust__class").value = "SINGLE";
   document.querySelector("#rep_cust__st").value = "NC";
+  document.querySelector("#rep_cust__addr1").value = items.cxStorage.cx_street;
+  document.querySelector("#rep_cust__ci").value = items.cxStorage.cx_city;
+  document.querySelector("#rep_cust__z").value = items.cxStorage.cx_zip;
 
   ///cx_number
   document.querySelector("#repair-header-card > div.repair-header__body > div:nth-child(1) > div.first-row__right-section > div.repair-customer.repair-customer--editable > div.repair-customer__form.el-row > div.repair-customer__left.el-col.el-col-24 > form:nth-child(1) > div > div.el-row > div > div > div.el-col.el-col-15 > div").className = "el-form-item is-success is-required is-no-asterisk el-form-item--mini";
@@ -37,9 +33,12 @@ var storedLegal = chrome.storage.local.get('cxStorage', function (items) {
   document.querySelector("#repair-header-card > div.repair-header__body > div:nth-child(1) > div.first-row__right-section > div.repair-customer.repair-customer--editable > div.repair-customer__form.el-row > div.repair-customer__right.el-col.el-col-24 > form > div > div.el-form-item.repair_class.is-error.is-required.is-no-asterisk.el-form-item--mini").className = "el-form-item repair_class is-success is-required is-no-asterisk el-form-item--mini";
   ///state
   document.querySelector("#customer\\.address\\[0\\]\\.stateCode").className = "el-form-item is-success is-required is-no-asterisk el-form-item--mini";
-
+  document.querySelector("#customer\\.address\\[0\\]\\.line1").className = "el-form-item is-success is-required is-no-asterisk el-form-item--mini";
+  document.querySelector("#customer\\.address\\[0\\]\\.city").className = "el-form-item is-success is-required is-no-asterisk el-form-item--mini";
+  document.querySelector("#customer\\.address\\[0\\]\\.postalCode").className = "el-form-item is-success is-required is-no-asterisk el-form-item--mini";
 
 
 console.log("PCRT info pasted into GSX");
 
 });
+
--- content_nomsg.js
+++ content_nomsg.js
@@ -5,10 +5,13 @@ var cx_email = document.querySelector("#contact-table > tbody > tr:nth-child(5) 
 var cx_pchorder = document.querySelector("#wo1content > div:nth-child(3) > table:nth-child(1) > tbody > tr:nth-child(1) > td:nth-child(2)").textContent+"GV";
 var cx_reference = document.querySelector("#woh2 > span:nth-child(2)").textContent;
 var cx_address = document.querySelector("#contact-table > tbody > tr:nth-child(6) > td:nth-child(2) > span").textContent;
-var cx_note = document.querySelector("#wo1content > table > tbody > tr:nth-child(1) > td:nth-child(2) > span");
+var cx_note = document.querySelector("#wo1content > table > tbody > tr:nth-child(1) > td:nth-child(2) > span").innerText;
 
 var cx_serial = document.querySelector("#vue-app > div:nth-child(5) > div:nth-child(2) > div:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(2) > span").textContent;
 
+var cx_street = document.querySelector("#checkout-form > input[type=hidden]:nth-child(3)").value;
+var cx_city = document.querySelector("#checkout-form > input[type=hidden]:nth-child(5)").value;
+var cx_zip = document.querySelector("#checkout-form > input[type=hidden]:nth-child(7)").value;
 
 var cx_note =
 console.log("source page ran");
@@ -20,9 +23,9 @@ var cxInfo = {
     cx_email: cx_email,
     cx_address: cx_address,
     cx_note: cx_note,
-    ///cx_addln: cx_addln,
-    ///cx_city: cx_city,
-    ///cx_zip: cx_zip,
+    cx_street: cx_street,
+    cx_city: cx_city,
+    cx_zip: cx_zip,
     ///cx_date: cx_date,
     cx_pchorder: cx_pchorder,
     cx_reference: cx_reference,
